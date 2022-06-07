a823aa7e0544e06a4280a1e2e96900ecb08599af
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-06-07 15:43:49 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10.c - a function that prints the alphabet 10 times 
+ * print_alphabet_x10.c - a function that prints the alphabet 10 times
  * Return: x10 a-z
  */
 
 void print_alphabet_x10(void)
 {
 	int n, co;
+
 	co = 0;
 
 	while (co < 10)
