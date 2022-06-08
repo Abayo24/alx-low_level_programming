d6b3d6504696e12ff03fdcf47beec9b7f61192f3
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-06-08 14:37:14 -0700

    d

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,18 +2,18 @@
 
 /**
  * main - gets sum of prime numbers
- * return: 0
+ * Return: 0
  */
 
 int main(void)
 {
-	int i, sum = 0;
+	unsigned long int i, sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 5 == 0) || (i % 3 == 0))
 		{
-			sum += 1;
+			sum = sum + i;
 		}
 	}
 	printf("%d\n", sum);
