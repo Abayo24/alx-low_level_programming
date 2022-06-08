3f36c6f74a1687288bb431dd2142ff2a1a97a2b7
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-06-08 15:09:13 -0700

    g

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,11 +9,13 @@ int main()
 {
 	int f0 = 1;
 	int f1 = 2;
+	int i;
+	int f2
 
-	for (int i = 1; i <=50; i++)
+	for (i = 3; i <=50; i++)
 	{
 		printf("%d\n", f1);
-		int f2 = f1 + f0;
+		f2 = f1 + f0;
 		f0 = f1;
 		f1 = f2;
 	}
