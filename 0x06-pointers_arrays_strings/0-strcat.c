a8c87b35fad4db6b94bb8c1016d58bdbbd9acc40
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-06-15 14:42:15 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int a;
-	int b;
+	int index = 0, dest_len = 0;
 
-	a = 0;
+	while (dest[index++])
+		dest_len++;
 
-	while (dest[a] != 0)
-	{
-		a++;
-	}
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
-	b = 0;
-
-	while (src[b] != 0)
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
 	return (dest);
 }
