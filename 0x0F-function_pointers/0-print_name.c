d38c1de8718adbf60b1243273d4e683d51a1869e
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-07-06 21:14:32 +0300

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
   * print_name - Prints a name
@@ -11,7 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL && f == NULL)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
