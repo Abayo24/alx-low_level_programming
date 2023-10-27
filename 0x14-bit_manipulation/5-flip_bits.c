4ed7a3b962c31e09e036279de07c020473fb9910
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2023-10-27 11:32:48 +0300

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,7 +4,7 @@
 /**
  * flip_bits - Write a function that returns the number of bits you would
  * need to flip to get from one number to another.
- * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m);
+ * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m)
  * You are not allowed to use the % or / operators
  * @n: Number Input 1.
  * @m: Number Input 2.
@@ -25,5 +25,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		if (comp != comp1)
 			flip += 1;
 	}
+
 	return (flip);
 }
