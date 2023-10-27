ce78ca66496b6f9da5f777d2301b412da2317693
Abayo <89282441+Abayo24@users.noreply.github.com>
2023-10-27 11:12:40 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,7 +6,7 @@
  * print_binary - Function that prints the binary representation of a number.
  * Prototype: void print_binary(unsigned long int n);
  * @n: number to convert to binary
- * You are not allowed to use arrays
+
  * You are not allowed to use malloc
  * You are not allowed to use the % or / operators
  */
