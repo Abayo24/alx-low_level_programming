6ba2cd7694291f4b6a2475d6f7b4b1b2d8f7ae9a
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2023-10-27 11:33:43 +0300

    update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,7 +4,7 @@
 /**
  * get_endianness - Write a function that checks the endianness.
  * Prototype: int get_endianness(void).
- * Return: 1, if architecture is little endian, 0 in case of big endian.
+ * return: 1, if architecture is little endian, 0 in case of big endian.
  */
 
 int get_endianness(void)
