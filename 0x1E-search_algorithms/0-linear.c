28b6b64530f600e759209d55e4f462be993de119
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-05-20 12:05:53 +0300

    Binary Search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,21 @@
  * Return: the first index where value is located if not present return -1
 */
 
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 
     size_t i;
 
-    if (array == NULL){
+    if (array == NULL)
+    {
         return (-1);
     }
 
-    for (i = 0; i < size; i++){
+    for (i = 0; i < size; i++)
+    {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value){
+        if (array[i] == value)
+        {
             return i;
         }
     }
